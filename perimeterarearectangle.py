98c0632faac5d384038ca6037e3f53d3e65af1b0
MohammedTahir <twahirmohammed786@gmail.com>
2024-12-10 16:39:57 +0530

    Create perimeter area of rectangle.py

---
+++ perimeterarearectangle.py
@@ -0,0 +1,7 @@
+perimeter and area of rectangle
+length=float(input('enter length: '))
+width=float(input('enter width: '))
+area=length*width
+perimeter=2*(length+width)
+print(area)
+print(perimeter)
