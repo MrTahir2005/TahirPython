8e1d6d19f7334fb860d5076b64001cab7ddde8db
MohammedTahir <twahirmohammed786@gmail.com>
2024-12-11 11:28:30 +0530

    Create CONTROL FLOW AND LOOPS Conditional Statements.py

---
+++ CONTROL FLOW AND LOOPS Conditional Statements.py
@@ -0,0 +1,26 @@
+CONTROL FLOW AND LOOPS Conditional Statements
+
+
+**IF** **STATEMENT**
+age=18
+if age>=18:
+  print("You are eligible to vote")
+
+# **if-else Statements**
+num=int(input("Enter a number: "))
+if num%2==0:
+  print("The number is even")
+else:
+  print("The number is odd")
+
+
+# **IF-ELIF-ELSE STATEMENT**
+Marks=int(input('Enter your marks:'))
+if Marks>=90:
+  print('Grade:A')
+elif Marks>=75:
+  print('Grade B:')
+elif Marks>=50:
+  print('Grade C:')
+else:
+  print('Grade F:')
